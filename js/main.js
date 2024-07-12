ec4028c8d5c05cdb6b7caf72308a3fd1f0202165
abdullahiyad <abdullahiyad10@gmail.com>
2024-07-12 10:03:38 +0300

    upload Calculator

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//change mode
+document.querySelector(".mode-btn").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("dark-theme");
+});
+
+//add event listener for each button and take the value from it
+document.querySelectorAll(".btn").forEach((ele) => {
+  ele.addEventListener("click", () => {
+    const screen = document.querySelector(".screen");
+    if (screen.textContent == "0") {
+      screen.textContent = ele.textContent;
+    } else {
+      screen.textContent = `${screen.textContent}${ele.textContent}`;
+    }
+  });
+});
+
+//ANS button function to get answer
+document.querySelector(".ans-btn").addEventListener("click", () => {
+  const screen = document.querySelector(".screen");
+  try {
+    const result = eval(screen.textContent.trim());
+    screen.textContent = result;
+  } catch (error) {
+    console.error("Error evaluating expression:", error);
+    screen.textContent = "Error";
+  }
+  //   screen.textContent = eval(screen.textContent);
+});
+
+//AC button to clear the screen
+document.querySelector(".clear-btn").addEventListener("click", () => {
+  document.querySelector(".screen").textContent = 0;
+});
